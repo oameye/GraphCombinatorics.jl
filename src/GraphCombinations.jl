4df608963c5e26d1a505ddfa537da4c2fee3c946
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-13 12:15:05 +0200

    test: enforce DispatchDoctor type stability

--- src/GraphCombinations.jl
+++ src/GraphCombinations.jl
@@ -4,38 +4,41 @@ $(DocStringExtensions.README)
 module GraphCombinations
 
 using DocStringExtensions
+using DispatchDoctor: @stable
 
 using Graphs, Multigraphs
 
-include("MultiGraphWrap.jl")
-include("utils.jl")
-include("partition_canonicalization.jl")
-include("matrix_canonicalization.jl")
-include("direct_generation.jl")
-include("row_state_reduction.jl")
-include("colored_port_generation.jl")
-include("colored_port_pruning.jl")
-include("colored_port_api.jl")
-include("generation.jl")
+@stable default_mode = "disable" default_codegen_level = "min" begin
+    include("MultiGraphWrap.jl")
+    include("utils.jl")
+    include("partition_canonicalization.jl")
+    include("matrix_canonicalization.jl")
+    include("direct_generation.jl")
+    include("row_state_reduction.jl")
+    include("colored_port_generation.jl")
+    include("colored_port_pruning.jl")
+    include("colored_port_api.jl")
+    include("generation.jl")
 
-export allgraphs, combinatoric_factor, build_graph, total_degree, canonical_form
-export ColoredPortEdge,
-    ColoredPortProblem,
-    ColoredPortState,
-    PortRelabeling,
-    WeightedPortCompletion,
-    PortGenerationStats,
-    AcceptAllPortPolicy,
-    MultiplicityPortTransport,
-    generate_weighted,
-    generate_weighted_with_stats,
-    generation_statistics,
-    canonical_relabeling,
-    relabel_port_state,
-    port_edges,
-    source_port_counts,
-    target_port_counts,
-    initial_port_weight,
-    transport_port_weight
+    export allgraphs, combinatoric_factor, build_graph, total_degree, canonical_form
+    export ColoredPortEdge,
+        ColoredPortProblem,
+        ColoredPortState,
+        PortRelabeling,
+        WeightedPortCompletion,
+        PortGenerationStats,
+        AcceptAllPortPolicy,
+        MultiplicityPortTransport,
+        generate_weighted,
+        generate_weighted_with_stats,
+        generation_statistics,
+        canonical_relabeling,
+        relabel_port_state,
+        port_edges,
+        source_port_counts,
+        target_port_counts,
+        initial_port_weight,
+        transport_port_weight
+end
 
 end # module GraphCombinations
