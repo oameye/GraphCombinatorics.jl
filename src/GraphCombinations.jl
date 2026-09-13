b8dcdf217d5a0358323d1aaa5cb8626055afa9a6
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-13 13:58:01 +0200

    refactor: inline production stability boundary

--- src/GraphCombinations.jl
+++ src/GraphCombinations.jl
@@ -8,7 +8,18 @@ using DispatchDoctor: @stable
 
 using Graphs, Multigraphs
 
-include("production.jl")
+@stable default_mode = "disable" default_codegen_level = "min" begin
+    include("MultiGraphWrap.jl")
+    include("utils.jl")
+    include("partition_canonicalization.jl")
+    include("matrix_canonicalization.jl")
+    include("direct_generation.jl")
+    include("row_state_reduction.jl")
+    include("colored_port_generation.jl")
+    include("colored_port_pruning.jl")
+    include("colored_port_api.jl")
+    include("generation.jl")
+end
 
 export allgraphs, combinatoric_factor, build_graph, total_degree, canonical_form
 export ColoredPortEdge,
